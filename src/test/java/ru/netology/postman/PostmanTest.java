ae17df81e359acf117cb9e5e379792df0324a3b1
Antonina <adrianery94@mail.ru>
2021-01-22 18:59:00 +0300

    Add Broken Test Commit

--- src/test/java/ru/netology/postman/PostmanTest.java
+++ src/test/java/ru/netology/postman/PostmanTest.java
@@ -19,7 +19,7 @@ public class PostmanTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some dataa"))
                 .contentType(ContentType.JSON);
     }
 }
